247bc68fbbb04e2333c0fa8c33193e51808c1cab
Kate Gordon <kategordon.code@gmail.com>
2019-11-06 09:34:29 -0500

    initial

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import Quote from "./components/quotes";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+    state = {
+        category: "dev"
+    };
+    render() {
+        const { category } = this.state;
+        return (
+            <div className="App">
+                <Quote category={category} />
+                <form action="#">
+                  <label>Select a Category</label>
+                  <button type='submit'>Get Quote for this Category</button>
+        </form>
+            </div>
+        );
+    }
 }
 
 export default App;
